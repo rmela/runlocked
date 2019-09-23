38110384e2c43be9f3d601f95fcc9ac761edbcc2
Robert Mela <robert.steven.mela@gmail.com>
2019-09-23 17:15:27 -0400

    Remove a lot of cruft that wasn't needed

--- index.js
+++ index.js
@@ -2,24 +2,12 @@ const lf = require('proper-lockfile');
 const fs = require('fs')
 const path = require('path')
 
-function _runUserFunc( func, resolve, reject ) {
-    return Promise.resolve()
-          .then( () => func() )
-          .then( resolve )
-          .catch( reject )
-}
-
-function runUserFunc( func ) {
-   return new Promise( (resolve, reject) => _runUserFunc( func, resolve, reject ) )
-}
-
 
 function Lock( directory, fname ) {
     this.lockDirectory = directory;
     this.lockfilePath = path.join( directory, fname );
     return this;
 }
-
 
 Lock.prototype.runlocked = function( func ) {
 
@@ -28,9 +16,10 @@ Lock.prototype.runlocked = function( func ) {
     return lf.lock( this.lockDirectory, { lockfilePath: this.lockFilePath } )
         .then( result => release = result )
         .then( function() { 
-             return runUserFunc( func ) 
-               .then( result => userResult = result )
-               .catch( err => userError = err )
+             return Promise.resolve()
+                 .then( () => func()  )
+                 .then( result => userResult = result )
+                 .catch( err => userError = err )
         })
         .then( () => release() )
         .then( () => userError ? Promise.reject( userError ) : userResult )
