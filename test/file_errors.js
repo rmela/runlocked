51b8ad035a007aca86d716d4723ed6c6385a0707
Robert Mela <robert.steven.mela@gmail.com>
2021-03-11 20:20:06 -0500

    Fix broken test

--- test/file_errors.js
+++ test/file_errors.js
@@ -3,13 +3,13 @@ const Lock = require('../index').Lock
 
 describe( "Lock", function() {
 
-   it( "foo", function(done)  {
+   it( "should not run user code if lockfile path is invalid", function(done)  {
 
       let userCodeRan = false
 
       lock = new Lock('nosuchdir', 'nosuchfile' )
 
-      lock.runlocked( () => ran = true )
+      lock.runlocked( () => userCodeRan = true )
          .catch( err => {
              assert.equal( err.code, 'ENOENT', "Error should be that directory doesn't exist" )
              assert.isFalse( userCodeRan, "User code should not have been run" )
